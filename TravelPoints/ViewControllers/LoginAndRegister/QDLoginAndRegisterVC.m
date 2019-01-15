bb4312443b804368fcc7f14dbbb1ba8d983c12be
ranj <ranjin@quantdo.com.cn>
2019-01-15 16:27:49 +0800

    新增注册登录相关页面

---
+++ TravelPoints/ViewControllers/LoginAndRegister/QDLoginAndRegisterVC.m
@@ -0,0 +1,32 @@
+//
+//  QDLoginAndRegisterVC.m
+//  TravelPoints
+//
+//  Created by 冉金 on 2019/1/15.
+//  Copyright © 2019年 Charles Ran. All rights reserved.
+//
+
+#import "QDLoginAndRegisterVC.h"
+
+@interface QDLoginAndRegisterVC ()
+
+@end
+
+@implementation QDLoginAndRegisterVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ TravelPoints/Views/LoginAndRegister/QDLoginView.h
@@ -0,0 +1,17 @@
+//
+//  QDLoginView.h
+//  TravelPoints
+//
+//  Created by 冉金 on 2019/1/15.
+//  Copyright © 2019年 Charles Ran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QDLoginView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
